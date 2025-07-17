e35b42831621db69eb8cecaee91466c52c02834b
Allen Hansen <me@allenhansen.dev>
2025-07-17 00:29:50 -0400

    lesson-jkfv0

--- courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/app/api/chat/route.ts
+++ courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/app/api/chat/route.ts
@@ -1,13 +1,28 @@
-import { streamText } from "ai";
+import type { Message } from "ai";
+import { streamText, createDataStreamResponse } from "ai";
 import { model } from "~/models";
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+export const maxDuration = 60;
 
-  const result = streamText({
-    model,
-    messages,
+export async function POST(request: Request) {
+  const body = (await request.json()) as {
+    messages: Array<Message>;
+  };
+
+  return createDataStreamResponse({
+    execute: async (dataStream) => {
+      const { messages } = body;
+
+      const result = streamText({
+        model,
+        messages,
+      });
+
+      result.mergeIntoDataStream(dataStream);
+    },
+    onError: (e) => {
+      console.error(e);
+      return "Oops, an error occured!";
+    },
   });
-
-  return result.toDataStreamResponse();
 }
